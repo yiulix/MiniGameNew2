20be0edd6389940ecc512909a60290ebfcf3132a
朱葛 <785930041@qq.com>
2019-07-18 14:06:04 +0800

    uu

---
+++ MiniGame/Assets/Scripts/FlowchartMangerBattle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowchartMangerBattle : MonoBehaviour
+{
+    public Fungus.Flowchart flowchart;
+    public BattleScene battle;
+    string[] toDialog;
+    void Start()
+    {
+        battle.BattleState = 0;
+    }
+
+   void SetbattleValue()
+    {
+        toDialog=battle.OneTurn();
+        flowchart.SetStringVariable("give", toDialog[0]);
+        flowchart.SetStringVariable("accept", toDialog[1]);
+        flowchart.SetStringVariable("damage", toDialog[2]);
+        flowchart.SetIntegerVariable("battlestate", battle.BattleState);
+
+    }
+}
