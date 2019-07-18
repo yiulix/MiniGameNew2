4da24fe1539c51ea7d1afb267d39a375c36cfc12
yrh6666 <52987311+yrh6666@users.noreply.github.com>
2019-07-18 11:01:04 +0800

    yrh

--- MiniGame/Assets/Scripts/FlowchartMangerBattle.cs
+++ MiniGame/Assets/Scripts/FlowchartMangerBattle.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class FlowchartMangerBattle : MonoBehaviour
 {
     public Fungus.Flowchart flowchart;
+    public BattleScene battle;
+    string[] toDialog;
     void Start()
     {
-        
+        battle.BattleState = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+   void SetbattleValue()
     {
-        
+        toDialog=battle.OneTurn();
+        flowchart.SetStringVariable("give", toDialog[0]);
+        flowchart.SetStringVariable("accept", toDialog[1]);
+        flowchart.SetStringVariable("damage", toDialog[2]);
+        flowchart.SetIntegerVariable("battlestate", battle.BattleState);
+
     }
 }
